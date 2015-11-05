43a0836b95e453da8677e118691f1aef949dff31
Michael Zekoff <zekoff@gmail.com>
2015-11-05 02:28:36 +0000

    Lay out hex grid and find adjacent tiles.

--- src/State/Main.js
+++ src/State/Main.js
@@ -1,7 +1,55 @@
+/* global game */
+
 var Main = {};
 
+var ROWS = 6;
+var COLUMNS = 6;
+
+var X_OFFSET = 200;
+var Y_OFFSET = 400;
+
 Main.create = function() {
-    print('created main');
+    var tiles = [];
+    var row, col;
+    for (col = 0; col < COLUMNS; col++) {
+        tiles.push([]);
+        for (row = 0; row < ROWS; row++) {
+            var tile = game.add.image(0, 0, 'pix');
+            tile.width = 100;
+            tile.height = 100;
+            tile.x = X_OFFSET + col * 120 + 60 * (row % 2);
+            tile.y = Y_OFFSET + row * 120;
+            tiles[col].push(tile);
+
+            tile.inputEnabled = true;
+            tile.events.onInputUp.add(function(pointer, target, unknown, row, col) {
+                print(col, row);
+                tiles.forEach(function(column) {
+                    column.forEach(function(e) {
+                        e.tint = 0xffffff;
+                    });
+                });
+                var adjacentTiles = [];
+                adjacentTiles.push(tiles[col - 1][row]);
+                adjacentTiles.push(tiles[col + 1][row]);
+                if (row % 2 === 0) {
+                    adjacentTiles.push(tiles[col - 1][row - 1]);
+                    adjacentTiles.push(tiles[col][row - 1]);
+                    adjacentTiles.push(tiles[col - 1][row + 1]);
+                    adjacentTiles.push(tiles[col][row + 1]);
+                }
+                else {
+                    adjacentTiles.push(tiles[col][row - 1]);
+                    adjacentTiles.push(tiles[col + 1][row - 1]);
+                    adjacentTiles.push(tiles[col][row + 1]);
+                    adjacentTiles.push(tiles[col + 1][row + 1]);
+                }
+                adjacentTiles.forEach(function(e) {
+                    e.tint = 0x00ff00;
+                });
+            }, tile, 0, row, col);
+        }
+    }
 };
 
 module.exports = Main;
