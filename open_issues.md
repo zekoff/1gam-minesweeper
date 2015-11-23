28937083a184b029deadc150a6c3d7861b3a413b
Michael Zekoff <zekoff@gmail.com>
2015-11-23 04:19:55 +0000

    Initial scrolling battle textarea.

--- open_issues.md
+++ open_issues.md
@@ -11,6 +11,7 @@ Implementing battles.
 - Make firing on marked targets do damage, etc.
 - Make firing on empty tiles destroy resources, etc.
 - Limit number of marked targets based on resources
+- Move player to adjacent tile before exploring a new tile
 
 ## Bugs
 
--- src/Element/Hud.js
+++ src/Element/Hud.js
@@ -8,6 +8,7 @@ var Hud = function() {
     this.inputMask = game.add.image(0, 0, 'pix');
     this.inputMask.width = 1080;
     this.inputMask.height = 1920;
+    this.inputMask.tint = 0x000000;
     this.inputMask.alpha = 0;
     this.wrapper.add(this.inputMask);
     this.wrapper.add(this);
@@ -69,11 +70,28 @@ var Hud = function() {
     mpBar.height = 80;
     this.add(mpBar);
 
+    this.textArea = game.add.image(20, 1940, 'pix');
+    this.textArea.width = 1040;
+    this.textArea.height = 700;
+    this.textArea.tint = 0xffffff;
+    this.add(this.textArea);
+
     // XXX debug only
     battleBackground.inputEnabled = true;
     battleBackground.events.onInputUp.add(function() {
-        ms.battle.victory();
+        // ms.battle.victory();
+        this.addText("You did " + game.rnd.between(10,20) + " damage!");
+        this.addText("The enemy did " + game.rnd.between(10,20) + " damage...");
     }, this);
+    var winButton = game.add.image(20, 2800, 'pix');
+    winButton.width = 1040;
+    winButton.height = 200;
+    winButton.inputEnabled = true;
+    winButton.events.onInputUp.add(function() {
+        ms.battle.victory();
+    });
+    winButton.tint = 0x00ff00;
+    this.add(winButton);
     // XXX end debug only
 };
 Hud.prototype = Object.create(Phaser.Group.prototype);
@@ -82,16 +100,64 @@ Hud.prototype.updateSurroundings = function(number) {
     this.surroundingEnemies.setText("Surrounded by: " + number);
 };
 Hud.prototype.startBattleMode = function() {
+    this.text = [];
+    if (this.content) this.content.destroy();
+    this.content = null;
+    this.addText("Battle!");
+
+    this.playerPlatform = game.add.image(270, 1440, ms.player.currentTile.key);
+    this.playerPlatform.anchor.set(0.5);
+    this.playerPlatform.height = 300;
+    this.playerPlatform.width = 500;
+    this.add(this.playerPlatform);
+    this.playerDisplay = game.add.image(270, 1440, 'pix');
+    this.playerDisplay.anchor.set(0.5, 1);
+    this.playerDisplay.width = ms.player.width * 2;
+    this.playerDisplay.height = ms.player.height * 2;
+    this.add(this.playerDisplay);
+
+    this.enemyPlatform = game.add.image(810, 1440, ms.battle.battleTile.key);
+    this.enemyPlatform.anchor.set(0.5);
+    this.enemyPlatform.height = 300;
+    this.enemyPlatform.width = 500;
+    this.add(this.enemyPlatform);
+    this.enemyDisplay = game.add.image(810, 1440, 'pix');
+    this.enemyDisplay.anchor.set(0.5, 1);
+    this.enemyDisplay.width = ms.player.width * 2;
+    this.enemyDisplay.height = ms.player.height * 2;
+    this.add(this.enemyDisplay);
+
     this.inputMask.inputEnabled = true;
+    this.inputMask.alpha = 0.4;
     this.y = -1200;
     this.markButton.x = -540;
     this.launchButton.x = 1080 + 540;
 };
 Hud.prototype.endBattleMode = function() {
+    this.playerPlatform.destroy();
+    this.playerDisplay.destroy();
+    this.enemyPlatform.destroy();
+    this.enemyDisplay.destroy();
     this.inputMask.inputEnabled = false;
+    this.inputMask.alpha = 0;
     this.y = 0;
     this.markButton.x = 360;
     this.launchButton.x = 720;
+};
+Hud.prototype.addText = function(text, color) {
+    this.text.push(text);
+    if (typeof color === 'undefined') color = 0x000000;
+    if (this.content)
+        this.content.destroy();
+    this.content = game.make.bitmapText(30, 2620, 'font', this.text.join('\n'), 60);
+    this.content.anchor.set(0, 1);
+    this.content.maxWidth = 1020;
+    this.content.tint = 0xcc8080;
+    while (this.content.height > this.textArea.height) {
+        this.text.shift();
+        this.content.setText(this.text.join('\n'));
+    }
+    this.add(this.content);
 };
 
 module.exports = Hud;
